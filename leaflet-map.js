cf9345b9a8d5326c73f17099711e1ee1faedfc83
arthurdoyle1 <150232794+arthurdoyle1@users.noreply.github.com>
2023-11-13 16:44:58 +1000

    Update leaflet-map.js

--- leaflet-map.js
+++ leaflet-map.js
@@ -1,6 +1,6 @@
 // Define custom icons for different categories
 var iconAdults = L.icon({
-    iconUrl: 'path/to/adults-icon.png',
+    iconUrl: 'https://github.com/arthurdoyle1/testdemo1/blob/main/military%20icon.png?raw=true',
     iconSize: [32, 32], // size of the icon
     iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
     popupAnchor: [0, -32] // point from which the popup should open relative to the iconAnchor
